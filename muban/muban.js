f4603acc6b3972b2530a766dd35707d39695b320
InMemory <272775028@qq.com>
2022-10-07 17:07:30 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -73,12 +73,14 @@ function 头部导航(){
           items.push({title:标题,url:分类地址,mode:"OKHTTP",翻页后:翻页后});
         }
     }else if(类型.indexOf("网页")!=-1){
-       for(var j=0;j<列表.length;j++){
-         var 标题=e2Rex(列表[j],标题规则);
-         var 地址=e2Rex(列表[j],地址规则).split("id/")[1].split(".html")[0];
-         var 分类地址=首页地址+前+地址+后;
-         items.push({title:标题,url:分类地址,mode:"JSOUP",翻页后:翻页后});
-       }
+        for(var j=0;j<列表.length;j++){
+          if(e2Rex(列表[j],地址规则).indexOf("id")!=-1){
+            var 标题=e2Rex(列表[j],标题规则);
+            var 地址=e2Rex(列表[j],地址规则).split("id/")[1].split(".html")[0];
+            var 分类地址=首页地址+前+地址+后;
+            items.push({title:标题,url:分类地址,mode:"JSOUP",翻页后:翻页后});
+          }
+        }
     }
     res.data=items;
     return JSON.stringify(res);
