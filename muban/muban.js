712389511eb100a5d0a12db1c2544fe45b2717ea
InMemory <272775028@qq.com>
2022-10-07 10:02:07 +0800

    更新 'muban/muban.js'

--- muban/muban.js
+++ muban/muban.js
@@ -17,7 +17,7 @@
 接口(app/v1/v2)-Dart/2.14 (dart:io)
 ######本地新增
 var key=getVar("输入内容");
-if(key.indexOf("==http")!=-1&&key.indexOf("@@")!=-1&&key.indexOf("--")!=-1&&key.indexOf("**")!=-1){
+if(key.indexOf("==http")!=-1&&key.indexOf("@@")!=-1&&key.indexOf("--")!=-1&&key.indexOf("**")!=-1&&key.indexOf("#KEY#")!=-1){
     var filename='站源.json';
     var 输入条目=key.match(/.+==http.+/g);
     for(var j=0;j<输入条目.length;j++){
