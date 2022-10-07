acfee8908f4e2ad2c5cb0e24e29fc1de779e7f72
InMemory <272775028@qq.com>
2022-10-07 17:52:44 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -79,7 +79,11 @@ function 头部导航(){
             var 地址=e2Rex(列表[j],地址规则).split("id/")[1].split(".html")[0];
             var 分类地址=首页地址+前+地址+后;
             items.push({title:标题,url:分类地址,mode:"JSOUP",翻页后:翻页后});
-          }
+          }else if(e2Rex(列表[j],地址规则).indexOf("vodtype")!=-1){
+              var 标题=e2Rex(列表[j],标题规则);
+              var 地址=e2Rex(列表[j],地址规则).split("vodtype/")[1].split(".html")[0];
+              var 分类地址=首页地址+前+地址+后;
+              items.push({title:标题,url:分类地址,mode:"JSOUP",翻页后:翻页后});
         }
     }
     res.data=items;
