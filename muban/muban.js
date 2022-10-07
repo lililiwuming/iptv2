6d9cc6d4e0dbe0ccb34439ccc3a6168009b83dd0
InMemory <272775028@qq.com>
2022-10-07 17:25:32 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -96,7 +96,7 @@ if(类型.indexOf("网页")!=-1){
         var 翻页后='.html';
         头部导航();
     }else if(类型.indexOf("MX Pro")!=-1){
-        var 列表=e2Arr(源码,".css(div.header-content div.nav ul li)");
+        var 列表=e2Arr(源码,".css(div.header-content div.nav ul li).or().css(div.header-content div.index-nav ul li)");
         var 标题规则=".t()";
         var 地址规则=".css(a).a(href)";
         var 前="/index.php/vod/show/id/";
