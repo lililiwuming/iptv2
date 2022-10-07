5e4e2a9a7654eba284160346e5f848c9a46c70fa
InMemory <272775028@qq.com>
2022-10-07 16:26:12 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -73,10 +73,9 @@ function 头部导航(){
           items.push({ title: 标题, url: 分类地址, mode: "OKHTTP", 翻页后: 翻页后 });
         }
     }else if(类型.indexOf("网页")!=-1){
-      alert(源码);
        for (var j = 0; j < 列表.length; j++) {
          var 标题 = e2Rex(列表[j], 标题规则) ? e2Rex(列表[j], 标题规则) : e2Rex(列表[j], 标题规则1);
-         var 地址 = e2Rex(列表[j], 地址规则).split("id")[1].split("page")[0];
+         var 地址 = e2Rex(列表[j], 地址规则);
          var 分类地址 = 首页地址 + 前 + 地址 + 后;
          items.push({ title: 标题, url: 分类地址, mode: "JSOUP", 翻页后: 翻页后 });
        }
