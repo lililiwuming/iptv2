e4a3f00559a9922b39105b8761e64176798ccb83
InMemory <272775028@qq.com>
2022-10-07 11:17:29 +0800

    更新 'muban/muban.js'

--- muban/muban.js
+++ muban/muban.js
@@ -29,7 +29,7 @@ key.indexOf("接口-APP(v2)")!=-1||key.indexOf("接口-APP(iptv)")!=-1)){
         var index=e2Rex(输入条目[j],".ty(==).tz(@@)");
         var search=e2Rex(输入条目[j],".ty(@@).tz(--)");
         var type=e2Rex(输入条目[j],".ty(--).tz(**)");
-        var UA=e2Rex(输入条目[j],".tz(**)");
+        var UA=e2Rex(输入条目[j],".ty(**)");
         记录.push({"站名":title,"首页地址":index,"搜索地址":search,"类型":type,UA:UA});
     }
     if(readStr(filename)){
