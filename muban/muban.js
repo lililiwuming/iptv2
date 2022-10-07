1f506f5897095dfe993cec9ba1a9fab4cb15ca93
InMemory <272775028@qq.com>
2022-10-08 00:28:52 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -309,11 +309,11 @@ if(类型.indexOf("xml")!=-1){
     var 状态规则='.tx(<p style=\"background-color:#CC00FF\"><font color=\"#FFFFFF\">).css(div.module-item-text).t().ct(</font></p>)';
     通用列表();
 }else if(类型.indexOf("MX")!=-1){
-    var 列表=e2Arr(源码.replace(/<.*?>/g,""),".json(list)");
-    var 标题规则=".json(vod_name)";
-    var 地址规则=".c(?ac=videolist&ids=).json(vod_id)";
-    var 图片规则=".json(vod_pic)";
-    var 播放源规则='.c(<font color=\"#0997F7\"><b>).json(vod_play_from).ct(</b></font><br>)';
-    var 状态规则='.tx(<p style=\"background-color:#CC00FF\"><font color=\"#FFFFFF\">).json(vod_remarks).ct(</font></p>)';
+    var 列表=e2Arr(源码,".css(ul.stui-vodlist.clearfix li)");
+    var 标题规则=".css(h3 a).a(title)";
+    var 地址规则=".css(h3 a).a(href)";
+    var 图片规则=".ct(p://43.140.205.222:4433/mxtheme/images/load.gif)";
+    var 播放源规则='.c(<font color=\"#0997F7\"><b>).css(span.time).t().ct(</b></font><br>)';
+    var 状态规则='.tx(<p style=\"background-color:#CC00FF\"><font color=\"#FFFFFF\">).css(em).t().ct(</font></p>)';
     通用列表();
 }
