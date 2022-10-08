b2e2688780886eecc2c70e1450e79a97dbf8dc9d
InMemory <272775028@qq.com>
2022-10-08 08:51:17 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -377,6 +377,7 @@ if(类型.indexOf("xml")!=-1){
     var URL=首页地址+"index_video"
     var 源码=getHttp(JSON.stringify({url:URL,redirect:false,head:{"User-Agent":UA}}));
     var 分类=e2Arr(源码.replace(/<.*?>/g,""),".json(list)";
+    alert(分类);
     var 分类标题规则=".json(type_name)";
     var 列表规则=".json(vlist)";
     var 标题规则=".json(vod_name)";
