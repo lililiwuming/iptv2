234ba5191ab570760346c50431985c0be495f019
InMemory <272775028@qq.com>
2022-10-08 15:54:28 +0800

    更新 'muban/muban.js'

--- muban/muban.js
+++ muban/muban.js
@@ -159,7 +159,7 @@ if(类型.indexOf("xml")!=-1){
     //var 列表=e2Arr(源码,".css(div.sidebar div ul>li)");
     var 列表=e2Arr(源码,".get(div.sidebar a[href~=/vod/type/.+])");
     var 标题规则=".get(a).t()";
-    var 地址规则=".get(a).a(href).z(\d)";
+    var 地址规则=".get(a).z2(\d)";
     var 前="/index.php/vod/show/id/";
     var 后="/page/";
     var 翻页后='.html';
