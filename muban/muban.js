34161b1a365e461f1626e70445133aec981ea27e
InMemory <272775028@qq.com>
2022-10-08 08:54:24 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -374,10 +374,10 @@ if(类型.indexOf("xml")!=-1){
     var 状态规则=".tx(<p style='background-color:#CC00FF'><font color='white'>).json(vod_remarks).or().json(type_name).ct(</font></p>)";
     通用列表();
 }else if(类型.indexOf("app")!=-1){
-    var URL=首页地址+"index_video"
+    var URL=首页地址+"index_video";
     var 源码=getHttp(JSON.stringify({url:URL,redirect:false,head:{"User-Agent":UA}}));
     var 分类=e2Arr(源码.replace(/<.*?>/g,""),".json(list)";
-    alert(URL);
+    alert(首页地址);
     var 分类标题规则=".json(type_name)";
     var 列表规则=".json(vlist)";
     var 标题规则=".json(vod_name)";
