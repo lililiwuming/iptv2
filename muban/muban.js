2dcebd162073991bffdc7df9a6dc98cbfca49faa
InMemory <272775028@qq.com>
2022-10-08 10:07:25 +0800

    更新 'muban/muban.js'

--- muban/muban.js
+++ muban/muban.js
@@ -435,9 +435,10 @@ if(类型.indexOf("xml")!=-1){
     通用列表();
 }else if(类型.indexOf("MXone Pro")!=-1){
     var 源码=getHttp(JSON.stringify({url:首页地址,redirect:false,head:{"User-Agent":UA}}));
-    var 分类=e2Arr(源码,".css(div.content div.module)");
     if(分类.indexOf("追剧周表")!=-1&&分类.indexOf("热榜")!=-1){
-        分类=e2Rex(分类,".i(1,-2)");
+        var 分类=e2Arr(源码,".css(div.content div.module).i(1,-2)");
+    }else{
+        var 分类=e2Arr(源码,".css(div.content div.module)");
     }
     var 分类标题规则=".css(h2).t()";
     var 列表规则=".css(div.module-items a)";
