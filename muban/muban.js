e9f3f54c352fd4cfb6809fac6379449b33b694c4
InMemory <272775028@qq.com>
2022-10-09 16:04:10 +0800

    更新 'muban/muban.js'

--- muban/muban.js
+++ muban/muban.js
@@ -651,6 +651,14 @@ if(类型.indexOf("xml")!=-1){
     var 选集规则='.json(title)';
     var 选集地址规则='.json(url)';
     选集列表();  
+}else if(类型.search("MXone Pro")!=-1){
+    var 简介=e2Arr(源,'.json(intro)');
+    var 分类=e2Arr(源,'.get(div.module-play-list)');
+    var 标题规则='.z2(\"\\(.*?\\)\")';
+    var 列表规则='.get(a[href~=/vodplay/.+])';
+    var 选集规则='.get(span).t()';
+    var 选集地址规则='.get(a).a(href)';
+    选集列表();  
 }
 ######播放规则10
 var uu=getVar("地址");
