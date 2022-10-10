5d7e26cd0c4168bd4d2939c85ecdd71178b0badf
InMemory <272775028@qq.com>
2022-10-11 05:05:48 +0800

    update

--- muban/muban.js
+++ muban/muban.js
@@ -595,6 +595,7 @@ var 类型=getVar("类型");
 var 首页地址=getVar("首页地址");
 var 搜索地址=getVar("搜索地址").replace("#KEY#",KEY).replace("#PN#",PN);
 var 源码=getHttp(JSON.stringify({url:首页地址+搜索地址,redirect:false,head:{"User-Agent":UA}}));
+alert(首页地址+搜索地址);
 function 搜索列表(){
     var res={};var items=[];var LIST=[];
     var LIMIT=列表.length;
